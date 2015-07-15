f92d17ca5aed7c2d3ed132210da25baefbf678fc
Jonatan Schumacher <jonatan@jschumacher.com>
2015-07-15 20:10:15 +0000

    init from C9.io

---
+++ config/environment.rb
@@ -0,0 +1,7 @@
+# Load the Rails application.
+require File.expand_path('../application', __FILE__)
+
+ENV['AWS_REGION'] = 'us-east-1'
+
+# Initialize the Rails application.
+Rails.application.initialize!
